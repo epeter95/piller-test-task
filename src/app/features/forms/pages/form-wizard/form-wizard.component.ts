a84ba36f916eb46a1302416bc6e8e3463b320731
erdosip <peter.erdosi@extremalelement.com>
2026-03-18 16:10:41 +0100

    form wizard stepper is implemented for both mobile and desktop devices

--- src/app/features/forms/pages/form-wizard/form-wizard.component.ts
+++ src/app/features/forms/pages/form-wizard/form-wizard.component.ts
@@ -1,17 +1,35 @@
 import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {FormService} from '../../services/form.service';
 import {toSignal} from '@angular/core/rxjs-interop';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, RouterLink} from '@angular/router';
+import {MatStep, MatStepLabel, MatStepper} from '@angular/material/stepper';
+import {MatButton} from '@angular/material/button';
+import {BreakpointObserver} from '@angular/cdk/layout';
+import {map} from 'rxjs';
+import {toSlug} from '../../../../shared/utilities/slug';
 
 @Component({
   selector: 'app-form-wizard',
-  imports: [],
+  imports: [
+    MatStep,
+    MatStepper,
+    MatStepLabel,
+    MatButton,
+    RouterLink
+  ],
   templateUrl: './form-wizard.component.html',
   styleUrl: './form-wizard.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FormWizardComponent {
   formService = inject(FormService);
+  breakpointObserver = inject(BreakpointObserver);
+  isMobile = toSignal(
+    this.breakpointObserver.observe('(max-width: 768px)')
+      .pipe(map(result => result.matches)),
+    {initialValue: false}
+  );
   slug = inject(ActivatedRoute).snapshot.params['slug'];
   form = toSignal(this.formService.getForm(this.slug));
+  protected readonly toSlug = toSlug;
 }
