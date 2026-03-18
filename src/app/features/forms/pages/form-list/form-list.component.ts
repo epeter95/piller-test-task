a71f050ad3e14fec9702c336a349ec076e7b45e9
erdosip <peter.erdosi@extremalelement.com>
2026-03-18 12:14:22 +0100

    form list table is implemented both desktop and mobile view

--- src/app/features/forms/pages/form-list/form-list.component.ts
+++ src/app/features/forms/pages/form-list/form-list.component.ts
@@ -3,17 +3,42 @@ import {FormService} from '../../services/form.service';
 import {toSignal} from '@angular/core/rxjs-interop';
 import {toSlug} from '../../../../shared/utilities/slug';
 import {RouterLink} from '@angular/router';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow,
+  MatHeaderRowDef,
+  MatRow,
+  MatRowDef,
+  MatTable
+} from '@angular/material/table';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-form-list',
   imports: [
-    RouterLink
+    RouterLink,
+    MatTable,
+    MatHeaderCell,
+    MatCell,
+    MatColumnDef,
+    MatCellDef,
+    MatHeaderCellDef,
+    MatHeaderRowDef,
+    MatHeaderRow,
+    MatRow,
+    MatRowDef,
+    MatButton
   ],
   templateUrl: './form-list.component.html',
   styleUrl: './form-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FormListComponent {
+  displayedColumns: Array<string> = ['name', 'stepsCount', 'status', 'action'];
   formService = inject(FormService);
   formList = toSignal(this.formService.getForms());
   protected readonly toSlug = toSlug;
